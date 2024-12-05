732cd265ff676ebf0f188b1fa4cd84b39eff2254
D4vel0per <omardavidmendezmontiel@gmail.com>
2024-12-05 15:05:58 -0400

    Start-blocking bugs fixed

--- src/hooks/useWspClient.tsx
+++ src/hooks/useWspClient.tsx
@@ -1,47 +1,13 @@
 import { useState, useEffect } from "react";
-import { formInfo, SenderCargo, SenderPkg, simpleMessage } from "../ws-bot/bot";
-import { RawData, WebSocket } from "ws";
+import { WebSocket } from "ws";
 
-interface SenderInfo<SenderPkg> {
-    cargo: SenderCargo,
-    package: SenderPkg
-}
-
-interface SenderResponse {
-    type: SenderCargo,
-    data: string,
-    from: string,
-    status: "SUCCESS"|"FAILED"
-}
-
-function rawToData (data: RawData) {
-    const info: SenderResponse = JSON.parse(data.toString());
-    const { STATE, PAIRING_CODE, MESSAGE } = SenderCargo;
-
-    if (info.status === "SUCCESS") {
-        let result: SenderInfo<SenderPkg> = {
-            cargo: info.type,
-            package: info.data
-        }
-        switch (info.type) {
-            case PAIRING_CODE:
-            case STATE: return result as SenderInfo<string>
-            case MESSAGE: return result as SenderInfo<simpleMessage>
-        }
-    }
-
-    return null
-}
-
-function dataToRaw (clientId:string, phoneNumber?:string): string {
-    let info: SenderInfo<formInfo> = {
-        cargo: SenderCargo.FORM,
-        package: {
-            clientId, phoneNumber
-        }
-    }
-    return JSON.stringify(info)
-}
+import { 
+    dataToRaw, 
+    rawToData, 
+    SenderCargo, 
+    SenderInfo, 
+    SenderPkg 
+} from "../ws-bot/bot";
 
 export default function useWspClient (socket: WebSocket, username: string, phoneNumber?:string) {
     const [ isFetched, setIsFetched ] = useState(false);
--- src/ws-bot/bot.ts
+++ src/ws-bot/bot.ts
@@ -1,47 +1,4 @@
-import { Client, LocalAuth, Message } from "whatsapp-web.js";
-import * as path from "path"
-import * as fs from "fs"
-
-const afs = fs.promises
-
-async function getClient (clientId: string) {
-    let dir = path.join("clients", clientId)
-    try {
-        await afs.access(dir, fs.constants.F_OK)
-    } catch {
-        console.log("Client", clientId, "was not found.")
-        return null
-    }
-    return new Client({
-        authStrategy: new LocalAuth({
-            clientId,
-            dataPath: dir
-        })
-    })
-}
-
-async function createClient (clientId: string, phoneNumber: string, sender: expoSender) {
-    let dir = path.join("clients", clientId)
-    let ifExists = await getClient(clientId)
-    if (ifExists) return ifExists
-    try {
-        fs.mkdirSync(dir);
-    } catch (err) {
-        console.log("ERROR CREATING CLIENT: ", err)
-    }
-
-    let client = new Client({
-        authStrategy: new LocalAuth({
-            clientId,
-            dataPath: dir
-        })
-    });
-    let sendCode = sender.bind(null, client, SenderCargo.PAIRING_CODE)
-    client.requestPairingCode(phoneNumber, true)
-    .then(pairingCode => sendCode(pairingCode))
-    
-    return client;
-}
+import { RawData } from "ws"
 
 enum SenderCargo {
     STATE = "State",
@@ -63,89 +20,54 @@ type formInfo = {
 
 type SenderPkg = string|simpleMessage|formInfo
 
-type expoSender = (client:Client, cargo: SenderCargo, pkg: SenderPkg) => any
-
-async function initClient (clientName: string, sender: expoSender) {
-    let client = await getClient(clientName)
-    if (!client) return clientName;
-
-    let show = sender.bind(null, client, SenderCargo.STATE)
-    let sendCode = sender.bind(null, client, SenderCargo.PAIRING_CODE)
-    let onMessage = sender.bind(null, client, SenderCargo.MESSAGE)
-
-    client.on("qr", async () => {
-        let pairingCode = await client.requestPairingCode(client.info.wid.user, true)
-        sendCode(pairingCode)
-    })
-    client.on("loading_screen", () => {
-        show("Preparing the spaceship...");
-        console.log("Loading...")
-    });
-    client.on("authenticated", () => {
-        show("Everyone on board...");
-        console.log("Succesfully Authenticated.")
-    });
-    client.on("auth_failure", (err) => {
-        show('Oh no, something wrong happened!');
-        console.log("Authentication failure: ", err)
-    })
-    client.on("ready", () => {
-        show("Let's go!")
-        console.log("Everything up and running... Ready to receive messages.")
-    })
-    client.on("message", (msj) => onMessage(msj))
-
-    try {
-        await client.initialize()
-        console.log("Client " + client.info.wid.user + " Initialized");
-        return client
-    } catch (err) {
-        console.log("Error while client initialazing "+ client.info.wid.user +" -> ", err)
-        return client.info.wid.user
-    }
+interface SenderInfo<SenderPkg> {
+    cargo: SenderCargo,
+    package: SenderPkg
 }
 
-async function runClients (
-    sender: expoSender
-) {
-    let clientNames = await afs.readdir("clients")
-    let proms = clientNames.map(clientName => initClient(clientName, sender))
+interface SenderResponse {
+    type: SenderCargo,
+    data: string,
+    from: string,
+    status: "SUCCESS"|"FAILED"
+}
 
-    let results = await Promise.allSettled(proms)
-    let initialValue: {
-        accepted: Client[],
-        rejected: string[]
-    } = {
-        accepted: [],
-        rejected: []
-    }
-    let allClients = results.reduce((obj, result) => {
-        if (result.status === "fulfilled") {
-            if (typeof result.value !== "string") 
-                obj.accepted.push(result.value)
-            else 
-                obj.rejected.push(result.value)
-        } else if (result.status === "rejected") {
-            console.log(result.reason)
+function rawToData (data: RawData) {
+    const info: SenderResponse = JSON.parse(data.toString());
+    const { STATE, PAIRING_CODE, MESSAGE } = SenderCargo;
+
+    if (info.status === "SUCCESS") {
+        let result: SenderInfo<SenderPkg> = {
+            cargo: info.type,
+            package: info.data
         }
-        return obj
-    }, initialValue)
+        switch (info.type) {
+            case PAIRING_CODE:
+            case STATE: return result as SenderInfo<string>
+            case MESSAGE: return result as SenderInfo<simpleMessage>
+        }
+    }
 
-    return allClients
+    return null
 }
 
-//HOW TO USE
-
-//FIRST OF ALL, YOU NEED TO HAVE AT LEAST 1 CLIENT FOR THIS FUNCTION TO WORK PROPERLY
-//THE CLIENT ID MUST BE THEIR PHONE NUMBER FOR EASIER 
+function dataToRaw (clientId:string, phoneNumber?:string): string {
+    let info: SenderInfo<formInfo> = {
+        cargo: SenderCargo.FORM,
+        package: {
+            clientId, phoneNumber
+        }
+    }
+    return JSON.stringify(info)
+}
 
 export {
-    expoSender,
     SenderCargo,
     SenderPkg,
+    SenderInfo,
+    SenderResponse,
     formInfo,
     simpleMessage,
-    getClient,
-    createClient,
-    initClient
+    rawToData,
+    dataToRaw
 }
